3094bdb2e41fd3d50ef26b69533236382e89f3d3
mikucy <419714107@qq.com>
2023-05-16 11:08:32 +0800

    Complete L1

--- kernel/src/os.c
+++ kernel/src/os.c
@@ -1,10 +1,12 @@
 #include <common.h>
+#include <klib-tests.h>
 
 static void os_init() {
   pmm->init();
 }
 
 static void os_run() {
+  klib_tests();
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
